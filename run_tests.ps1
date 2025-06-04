ddf68fdf263e82d9e444085d8025a11c1cd5fa09
Sina Reidenbach <buero.reidenbach@gmail.com>
2025-06-04 13:36:42 +0200

    output folder limited 10max

--- run_tests.ps1
+++ run_tests.ps1
@@ -3,10 +3,21 @@ if (-not (Test-Path -Path "results")) {
 }
 
 $timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
-
-$resultsPath = "results\$timestamp"
-New-Item -ItemType Directory -Path $resultsPath
+$resultsPath = "$resultsRoot\$timestamp"
+New-Item -ItemType Directory -Path $resultsPath | Out-Null
 
 robot --output "$resultsPath/output.xml" --log "$resultsPath/log.html" --report "$resultsPath/report.html" tests/
 Write-Host "Tests wurden ausgeführt in: $resultsPath"
 Start-Process "$resultsPath\log.html"
+
+$maxFolders = 10
+$resultsRoot = "results"
+
+$folders = Get-ChildItem $resultsRoot -Directory | Sort-Object LastWriteTime
+
+if ($folders.Count -gt $maxFolders) {
+    $toDelete = $folders.Count - $maxFolders
+    $folders | Select-Object -First $toDelete | ForEach-Object {
+        Remove-Item -Recurse -Force $_.FullName
+    }
+}
